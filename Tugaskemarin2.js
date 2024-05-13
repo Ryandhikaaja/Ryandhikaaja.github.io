9c00b518d2f3377759868e483f26c6df60b044be
Ryandhikaaja <168519193+Ryandhikaaja@users.noreply.github.com>
2024-05-13 13:52:01 +0700

    Create Tugaskemarin2.js

---
+++ Tugaskemarin2.js
@@ -0,0 +1,35 @@
+var x="nama";
+    x+=15;
+ console.log(x)
+ 
+ var x="buku";
+    x+=5;
+ console.log(x)
+ 
+ var x="apel";
+    x+=10;
+ console.log(x)
+ 
+ var x="baju";
+    x+=3;
+ console.log(x)
+ 
+ var x="saya";
+    x+=1;
+ console.log(x)
+
+
+
+
+
+
+
+var x={nama:"Lana",umur:23};
+console.log(x.umur)
+
+var x={nama:"Bagas",umur:15,absen:6,kelas:"rpl3"};
+console.log(x)
+console.log(x.nama)
+console.log(x.umur)
+console.log(x.absen)
+console.log(x.kelas)
