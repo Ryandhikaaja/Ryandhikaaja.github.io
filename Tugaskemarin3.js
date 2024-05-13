53ff8ac19e9351aa3c16ee1a150426d6d554da99
Ryandhikaaja <168519193+Ryandhikaaja@users.noreply.github.com>
2024-05-13 13:53:34 +0700

    Create Tugaskemarin3.js

---
+++ Tugaskemarin3.js
@@ -0,0 +1,44 @@
+//let//
+
+let x= 34 + 14
+ x= 10 + 54
+console.log(x)
+
+ x= 260 + 17
+ x+= 49 + 78
+console.log(x)
+
+y= 40 + 10
+ y*= 11 + 3
+console.log(y)
+
+z= 27 - 25
+ z!= 45 + 32
+console.log(z)
+
+x= 156 * 19
+ x/= 20 + 28
+console.log(x)
+
+//var//
+
+var y= 189 + 14
+var y= 34 + 106
+console.log(y)
+
+var y= 10 + 78
+var y= 11 + 10
+console.log(y)
+
+var z= 18 + 23
+var z= 511 + 109
+console.log(z)
+
+var m= 16 + 54
+var m= 15 + 67
+console.log(m)
+
+var w= 19 + 346
+var w= 18 + 167
+console.log(w)
+  
